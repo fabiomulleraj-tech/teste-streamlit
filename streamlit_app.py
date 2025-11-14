6853ead4ec54cbdfc78fb489019dabbe85580c95
fabiomulleraj-tech <fabiomuller.aj@gmail.com>
2025-11-14 17:22:43 +0000

    correção cookies sessão

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,6 +16,12 @@ from ldap3 import Server, Connection, ALL, SIMPLE, Tls
 
 st.set_page_config(page_title="Bentinho", page_icon="❄️", layout="wide")
 
+st.session_state.setdefault("logged_in", False)
+st.session_state.setdefault("username", None)
+
+st._config.set_option("server.cookieSameSite", "lax")
+st._config.set_option("server.cookieSecure", False)
+
 # ---------------------------------------------------------
 # SESSION INITIALIZATION
 # ---------------------------------------------------------
